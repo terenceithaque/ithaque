a1aadb2ae41f6fae14e022c4d4d78997be89a150
unknown <terence.lethierry@protonmail.com>
2022-01-12 21:15:00 +0100

    flèche directionelle gauche

--- js/navigationFleche.js
+++ js/navigationFleche.js
@@ -1,6 +1,13 @@
-function definirNavigationFleche(nomPage) {
+function definirNavigationFlecheDroite(nomPage) {
   window.addEventListener("keydown", function (e) {
     if (e.key === "ArrowRight") {
+      window.location = nomPage;
+    }
+  });
+}
+function definirNavigationFlecheGauche(nomPage) {
+  window.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
       window.location = nomPage;
     }
   });
